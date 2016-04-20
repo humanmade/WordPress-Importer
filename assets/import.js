e20e5446c708b7c4c4ccb996a49f737896380411
Herb Miller <herb@bobbingwide.com>
2016-04-20 15:58:17 +0100

    Fixes #40 NaN% for total 0

--- assets/import.js
+++ assets/import.js
@@ -19,10 +19,11 @@
 		updateProgress: function ( type, complete, total ) {
 			var text = complete + '/' + total;
 			document.getElementById( 'completed-' + type ).innerHTML = text;
+      total = parseInt( total );
       if ( 0 == total ) {
         total = 1;
       }
-  		var percent = parseInt( complete ) / parseInt( total );
+  		var percent = parseInt( complete ) / total;
 			document.getElementById( 'progress-' + type ).innerHTML = Math.round( percent * 100 ) + '%';
 			document.getElementById( 'progressbar-' + type ).value = percent * 100;
 		},
